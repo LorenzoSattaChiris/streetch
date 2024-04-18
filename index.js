5bf31f23a1bd0be20101bd25fb91d539bd35830d
LorenzoSattaChiris <ls1044@exeter.ac.uk>
2024-04-18 14:26:44 +0200

    Push2

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
 
-// app.get('/bom', (req, res) => {
-//     res.redirect('https://lsattachiris.notion.site/IMECHE-STREETCH-BOM-975d8fde7d7a442c84cb670cf4722836?pvs=4');
-// });
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the public directory
 
-// app.get('/poster', (req, res) => {
-//     res.redirect('https://www.canva.com/design/DAGCftDCVQQ/hcSXp3mZOZXagdH0uZIf2g/view?utm_content=DAGCftDCVQQ&utm_campaign=designshare&utm_medium=link&utm_source=editor');
-// });
+app.get('/bom', (req, res) => {
+    res.redirect('https://lsattachiris.notion.site/IMECHE-STREETCH-BOM-975d8fde7d7a442c84cb670cf4722836?pvs=4');
+});
 
-// app.get(['/video', '/presentation'], (req, res) => {
-//     res.redirect('https://www.loom.so');
-// });
+app.get('/poster', (req, res) => {
+    res.redirect('https://www.canva.com/design/DAGCftDCVQQ/hcSXp3mZOZXagdH0uZIf2g/view?utm_content=DAGCftDCVQQ&utm_campaign=designshare&utm_medium=link&utm_source=editor');
+});
 
-// app.get('/cad', (req, res) => {
-//     res.sendFile(__dirname + '/cad.html');
-// });
+app.get(['/video', '/presentation'], (req, res) => {
+    res.redirect('https://www.loom.so');
+});
 
-// app.get(['/dijkstra', '/path'], (req, res) => {
-//     res.sendFile(__dirname + '/dijkstra.html');
-// });
+app.get('/cad', (req, res) => {
+    res.sendFile(__dirname + '/cad.html');
+});
 
-// app.get(['/code', '/arduino'], (req, res) => {
-//     res.sendFile(__dirname + '/code.html');
-// });
+app.get(['/dijkstra', '/path'], (req, res) => {
+    res.sendFile(__dirname + '/dijkstra.html');
+});
 
-// app.get('*', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
+app.get(['/code', '/arduino'], (req, res) => {
+    res.sendFile(__dirname + '/code.html');
+});
 
-app.use(express.static('.')); // Serve all static files in the directory
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
